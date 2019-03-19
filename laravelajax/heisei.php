9fdc9b81383413885d2c6d33793713e6f847fa4f
T-sugo <48705572+T-sugo@users.noreply.github.com>
2019-03-19 14:09:23 +0900

    Create heisei.php

---
+++ laravelajax/heisei.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset=utf-8">
+<title></title>
+<script type="text/javascript" src="js/jquery-2.2.3.min.js"></script>
+<script type="text/javascript" src="js/heisei.js"></script>
+</head>
+
+<body>  
+  <h2>西暦を平成○○年に変換します（2016年現在）</h2>
+  <input text id="seireki" name="seireki" style="width:50px"> 年 <br><br>
+  <input type="submit"  id="send" value="平成に変換する！" >  
+</body>
+</html>
