710b621d1275f823e9a7db1cc48a68f1ce201a73
T-sugo <48705572+T-sugo@users.noreply.github.com>
2019-03-19 14:14:36 +0900

    Create routes.php

---
+++ cakephp/routes.php
@@ -0,0 +1,4 @@
+// ページを表示するコントローラー
+$routes->connect('/dataadd',['controller' => 'Dataadd', 'action' => 'dataadd']);
+// データを登録するコントローラー
+$routes->connect('/add',['controller' => 'Add', 'action' => 'add']);
